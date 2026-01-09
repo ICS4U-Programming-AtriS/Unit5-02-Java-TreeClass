cfe9cd5876063759158a4bb5964adee292301ceb
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2026-01-09 10:47:01 -0500

    Initial commit

---
+++ NameOfProgram.java
@@ -0,0 +1,26 @@
+/**
+* <Description>
+*
+* @author  Atri Sarker
+* @version 1.0
+* @since   2025-Month-Day
+*/
+public final class NameOfProgram {
+  /**
+   * Private constructor to satisfy style checker.
+   * @exception IllegalStateException for the utility class.
+   * @see IllegalStateException
+   */
+  private NameOfProgram() {
+    // Prevents illegal states.
+    throw new IllegalStateException("Utility class.");
+  }
+
+  /**
+   * Entrypoint of the program.
+   * @param args UNUSED.
+   */
+  public static void main(final String[] args) {
+    // CODE GOES HERE
+  }
+}
---
+++ package-info.java
@@ -0,0 +1,7 @@
+/**
+* package-info file for NameOfProgram.
+*
+* @author  Atri Sarker
+* @version 1.0
+* @since   2025-Month-Day
+*/
